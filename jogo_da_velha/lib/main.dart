6d223ccb0f85c1a52c1ccfb3ece4d895b8a137db
CamilaFernandesdev <camila.fernandes.fayer@gmail.com>
2025-01-27 15:41:09 -0300

    copy idx project

--- jogo_da_velha/lib/main.dart
+++ jogo_da_velha/lib/main.dart
@@ -1,18 +1,165 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const TicTacToeApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TicTacToeApp extends StatelessWidget {
+  const TicTacToeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TicTacToeGame(),
+    );
+  }
+}
+
+class TicTacToeGame extends StatefulWidget {
+  const TicTacToeGame({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TicTacToeGameState createState() => _TicTacToeGameState();
+}
+
+class _TicTacToeGameState extends State<TicTacToeGame> {
+  List<String> board = List.filled(9, '');
+  String currentPlayer = 'X';
+  String winner = '';
+
+  void resetGame() {
+    setState(() {
+      board = List.filled(9, '');
+      currentPlayer = 'X';
+      winner = '';
+    });
+  }
+
+  void makeMove(int index) {
+    if (board[index].isEmpty && winner.isEmpty) {
+      setState(() {
+        board[index] = currentPlayer;
+        if (checkWinner()) {
+          winner = currentPlayer;
+        } else {
+          currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        }
+      });
+    }
+  }
+
+  bool checkWinner() {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var pattern in winningPatterns) {
+      if (board[pattern[0]] == currentPlayer &&
+          board[pattern[1]] == currentPlayer &&
+          board[pattern[2]] == currentPlayer) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      appBar: AppBar(
+        backgroundColor: Colors.purple[100],
+        title: const Text(
+          'TIC TAC TOE',
+          style: TextStyle(
+            letterSpacing: 2,
+            fontWeight: FontWeight.w600,
+            color: Colors.purple
+          ),),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20,),
+            GridView.builder(
+              
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                childAspectRatio: 0.5,
+                mainAxisExtent: 80
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => makeMove(index),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      // border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        board[index],
+                        style: TextStyle(
+                          color: Colors.purple[200],
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 20.0),
+            if (winner.isNotEmpty)
+              Text(
+                'Vencedor: $winner',
+                style: const TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            if (winner.isEmpty && board.every((cell) => cell.isNotEmpty))
+              const Text(
+                'Empate!',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            const SizedBox(height: 20.0),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.purpleAccent,
+                side: BorderSide(
+                  width: 2.5,
+                  color: Colors.purpleAccent)
+              ),
+              onPressed: resetGame,
+              child: const Text("Reiniciar Jogo")),
+            
+            
+          ],
         ),
       ),
     );
